d799bf81bf5400c1bb1d71cf4cfe0a0a64c24405
jon conner <jconnergoldleafpres@gmail.com>
2022-07-11 11:40:55 -0700

    Add project files.

---
+++ Method Overloading/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Method_Overloading
+{
+    internal class Program
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            var sum = 0;
+            if (isTrue)
+            {
+                sum = num1 + num2;
+                if (sum )
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
